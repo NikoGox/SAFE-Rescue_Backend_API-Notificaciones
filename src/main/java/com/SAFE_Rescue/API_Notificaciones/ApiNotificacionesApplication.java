a208398da3723c4655ce436f7b32f4f14019f232
Rubén Parada Peña <nico.94lta@gmail.com>
2025-05-25 22:45:05 -0400

    Inicializacion repositorio API Notificaciones

---
+++ src/main/java/com/SAFE_Rescue/API_Notificaciones/ApiNotificacionesApplication.java
@@ -0,0 +1,13 @@
+package com.SAFE_Rescue.API_Notificaciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiNotificacionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiNotificacionesApplication.class, args);
+	}
+
+}
